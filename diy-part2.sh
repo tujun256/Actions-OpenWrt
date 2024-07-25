67fbe3d42258cb206a5903ee74d1f5942f00dd38
tujun256 <tujun256@foxmail.com>
2024-07-25 08:08:53 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,30 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+df -h
+
+#append_to_mk
+FILE="${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/image/mt7986.mk"
+
+# 定义要追加的内容
+CONTENT=$(cat <<'END'
+define Device/mt7986a-2500wan-emmc-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986a-2500wan-emmc-rfb
+  DEVICE_DTS := mt7986a-2500wan-emmc-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986a-emmc-rfb
+  DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
+                     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
+                     kmod-nls-iso8859-1
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986a-2500wan-emmc-rfb
+END
+)
+
+# 将内容追加到文件末尾
+echo "$CONTENT" >> "$FILE"
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
