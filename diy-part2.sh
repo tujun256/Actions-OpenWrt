c06b9a25ae7b0fb233285ccb83e5f0288dd60254
tujun256 <tujun256@foxmail.com>
2024-07-25 09:43:13 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -49,6 +49,6 @@ cp -af ./feeds/mtk_openwrt_feed/21.02/files/* .
 
 cp -af ./feeds/mtk_openwrt_feed/tools .
 
-for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-base -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
+#for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-base -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
 
-for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-feeds -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
+#for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-feeds -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
