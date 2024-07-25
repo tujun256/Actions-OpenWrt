99edc73e1a89e896aa516960a6e02784c4b381be
tujun256 <tujun256@foxmail.com>
2024-07-25 16:41:25 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,19 +16,6 @@ FILE="${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/image/mt7986.mk"
 
 # 定义要追加的内容
 CONTENT=$(cat <<'END'
-define Device/mt7986a-2500wan-emmc-rfb
-  DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7986a-2500wan-emmc-rfb
-  DEVICE_DTS := mt7986a-2500wan-emmc-rfb
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7986a-emmc-rfb
-  DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
-                     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
-                     kmod-nls-iso8859-1
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += mt7986a-2500wan-emmc-rfb
-
 define Device/mt7986a-emmc-rfb
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := mt7986a-emmc-rfb
