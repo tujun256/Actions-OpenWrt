ad6a002b9b8239ec32d69a2e014f0b0c0648a2e8
tu <tujun256@foxmail.com>
2024-07-25 20:28:41 +0800

    modify yml file

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,29 +11,29 @@
 #
 df -h
 
-#append_to_mk
-FILE="${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/image/mt7986.mk"
+# #append_to_mk
+# FILE="${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/image/mt7986.mk"
 
-# 定义要追加的内容
-CONTENT=$(cat <<'END'
-define Device/mt7986a-emmc-rfb
-  DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7986a-emmc-rfb
-  DEVICE_DTS := mt7986a-emmc-rfb
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7986a-emmc-rfb
-  DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
-                     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
-                     kmod-nls-iso8859-1
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += mt7986a-emmc-rfb
+# # 定义要追加的内容
+# CONTENT=$(cat <<'END'
+# define Device/mt7986a-emmc-rfb
+#   DEVICE_VENDOR := MediaTek
+#   DEVICE_MODEL := mt7986a-emmc-rfb
+#   DEVICE_DTS := mt7986a-emmc-rfb
+#   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+#   SUPPORTED_DEVICES := mediatek,mt7986a-emmc-rfb
+#   DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
+#                      kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
+#                      kmod-nls-iso8859-1
+#   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+# endef
+# TARGET_DEVICES += mt7986a-emmc-rfb
 
-END
-)
+# END
+# )
 
-# 将内容追加到文件末尾
-echo "$CONTENT" >> "$FILE"
+# # 将内容追加到文件末尾
+# echo "$CONTENT" >> "$FILE"
 
 
 # Modify default IP
